b706072e28e42ee46486068c35960003d3c15885
eman saeed <emansaeedezz328@gmail.com>
2025-04-03 06:18:22 +0200

    + Add SplashScreen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -7,7 +7,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+appBar: AppBar(
+  title:Text('Islami'),
+  backgroundColor: Colors.pink,
+),
     );
   }
 }
