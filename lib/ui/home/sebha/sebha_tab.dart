3624fc421b15982040a8da931b74f82ca7f941a4
eman saeed <emansaeedezz328@gmail.com>
2025-04-06 07:11:26 +0200

    Merge pull request #1 from emansaeed328/feat/splash_screen
    Feat/splash screen

---
+++ lib/ui/home/sebha/sebha_tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SebhaTab extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Placeholder(
+      color: Colors.blue,
+    );
+  }
+
+}
